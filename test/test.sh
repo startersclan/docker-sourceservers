c51893eef51db3f8919c7ff013359bbdb6995c42
Joel Timothy Oh <joeltimothyoh@live.com>
2022-10-08 22:29:32 +0000

    Improve check for game version

--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -eu
 
-for i in $( ls hlds_* ); do echo "[$i]"; cat "$i" | grep -iE 'exe|version' | sed 's/[^0-9]//g'; echo; done
-for i in $( ls srcds_* ); do echo "[$i]"; cat "$i" | grep -iE 'exe|version' | sed 's/[^0-9]//g'; echo; done
+for i in $( ls hlds_* ); do echo "[$i]"; cat "$i" | grep -iE '\bexe\b|version' | sed 's/[^0-9]//g'; echo; done
+for i in $( ls srcds_* ); do echo "[$i]"; cat "$i" | grep -iE '\bexe\b|version' | sed 's/[^0-9]//g'; echo; done
