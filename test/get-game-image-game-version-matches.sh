d08f5168c8a11c4a4b5aaeefe447f3f9704ff6b4
Joel Timothy Oh <joeltimothyoh@live.com>
2022-10-09 09:32:40 +0000

    Rename game image test log files

--- test/get-game-image-game-version-matches.sh
+++ test/get-game-image-game-version-matches.sh
@@ -4,5 +4,5 @@ set -eu
 BASE_DIR=$( dirname "$0" | xargs realpath )
 cd "$BASE_DIR"
 
-for i in $( ls hlds_* ); do echo "[$i]"; cat "$i" | grep -iE '\bexe\b|version' | sed 's/[^0-9]//g'; echo; done
-for i in $( ls srcds_* ); do echo "[$i]"; cat "$i" | grep -iE '\bexe\b|version' | sed 's/[^0-9]//g'; echo; done
+for i in $( ls hlds-* ); do echo "[$i]"; cat "$i" | grep -iE '\bexe\b|version' | sed 's/[^0-9]//g'; echo; done
+for i in $( ls srcds-* ); do echo "[$i]"; cat "$i" | grep -iE '\bexe\b|version' | sed 's/[^0-9]//g'; echo; done
