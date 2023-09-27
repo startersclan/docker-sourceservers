6ebd1b2568cc54ad0a98fea896c432e2edfe91a6
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2023-09-27 13:32:29 +0000

    Fix examples

--- Generate-GitBranches.ps1
+++ Generate-GitBranches.ps1
@@ -3,16 +3,16 @@
 # 3. To build a game, checkout to its branch, edit .env, mutate .trigger, commit and push
 # Examples:
 #   # Create branches for all games
-#   ./ Generate-GitBranches.ps1 -TargetRepo . -Pull
+#   ./Generate-GitBranches.ps1 -TargetRepo . -Pull
 #
 #   # Create branches for specific game
-#   ./ Generate-GitBranches.ps1 -TargetRepo . -Pull -GameEngine srcds -Game csgo
+#   ./Generate-GitBranches.ps1 -TargetRepo . -Pull -GameEngine srcds -Game csgo
 #
 #   # Update branches for all games
-#   ./ Generate-GitBranches.ps1 -TargetRepo . -Pull -Push
+#   ./Generate-GitBranches.ps1 -TargetRepo . -Pull -Push
 #
 #   # Update branches for specific game
-#   ./ Generate-GitBranches.ps1 -TargetRepo . -Pull -Push -GameEngine srcds -Game csgo
+#   ./Generate-GitBranches.ps1 -TargetRepo . -Pull -Push -GameEngine srcds -Game csgo
 #
 [CmdletBinding()]
 param(
